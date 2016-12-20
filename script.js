cf5cf388144a49a4ff6be67e6f99693e1e2cde55
Jane Philipps <jane.philipps@gmail.com>
2016-12-20 13:22:12 -0800

    clean up code

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-// Fibonacci Infinity Scarf
-// This code generates data for an "infinity" scarf based on the Fibonacci sequence
+/* Standard scarf sizes are 772 x 84 pixels or 1120 x 176 pixels (1 pixel = 1 knit stitch)
+ * 772 * 84 = 64848 => separate(toBinary(makeArray(433))) to get 64948
+ * 1120 * 176 = 197120 => separate(toBinary(makeArray(755))) to get 197632
+ */
 
-// TODO: Write a function where user passes in width(stitches) and height(rows) and outputs:
-// {
-//   width: width,
-//   height: height,
-//   data : [0,1,0,0,1,1, .... in length of width * height]
-// }
-
-
-// Standard scarf sizes are 772 x 84 pixels or 1120 x 176 pixels (1 pixel = 1 knit stitch)
-// 772 * 84 = 64848 => pass in 433 below to get 64948
-// 1120 * 176 = 197120 => pass in 755 below to get 197632
-
-// Recursive Fibonacci function
+// Recursive Fibonacci function for development/reference
 var fibonacci = function(n) {
 
   // base case #1
@@ -29,35 +19,35 @@ var fibonacci = function(n) {
 
 // Store Fibonacci numbers in array
 var makeArray = function(n) {
-  console.log('making array');
   let arr = [];
   for (let i = 1; i <= n; i++) {
+    /* Using the recursive function above is slow,
+     * so instead, using the array this function is creating
+     * to find the next number in the sequence
+     */
     // arr.push(fibonacci(i));
     if (i === 1 || i === 2) {
       arr.push(1);
     } else {
       let f = arr[i - 2] + arr[i - 3];
+      // Handles infinity case
       if (isFinite(f)) {
         arr.push(f);
       } else {
-        console.log('infinity reached - array cut short');
+        console.log('Infinity reached, array cut short.');
         return arr;
       }
     }
   }
-  console.log('done making array');
   return arr;
 }
 
 // Return array of binary numbers
 var toBinary = function(a) {
-  console.log('getting binary numbers');
   let arr = [];
-
   for (let i = 0; i < a.length; i++) {
     arr.push(convert(a[i]));
   }
-  console.log('finished binary');
   return arr;
 }
 
@@ -74,7 +64,7 @@ var convert = function(n) {
     binary.unshift(remainder);
   }
 
-  // If using 8-bit etc.
+  // If using 8-bit binary etc., this adds 0s to beginning
   // while (binary.length < 8) {
   //   binary.unshift('0');
   // }
@@ -85,48 +75,43 @@ var convert = function(n) {
 
 // Separate array of binary numbers into individual comma separated numbers
 var separate = function(a) {
-  console.log('separating');
   let s = a.join('');
   let arr = [];
-
   for (let i = 0; i < s.length; i++) {
     arr.push(parseInt(s[i]));
   }
-  console.log('separated');
-
   return arr;
-
 }
-
-// Create the array for "data" attr in above object
-// var arr = separate(toBinary(makeArray(754)));
 
 // Given width and height, output data
 var createScarf = function(width, height) {
-  console.log('creating scarf');
+  let area = width * height;
+  let arr = [];
   let data = {};
+  let extra;
+  let newArr;
+  let start = 1;
+
   data.width = width;
   data.height = height;
-  let area = width * height;
 
-  let start = 1;
-  let arr = [];
-  // 1476 is largest number before infinity
+  // 1476 is largest number before Fibonacci sequence reaches infinity
   for (let i = start; i < 1476; i++) {
     arr = separate(toBinary(makeArray(i)));
-    if (arr.length < area) {
-      console.log('Not long enough: ', i, arr.length, area);
-    } else {
-      console.log('Long enough: ', i, arr.length, area);
-      break;
-    }
+    // Logging for testing
+    // if (arr.length < area) {
+    //   console.log('Not long enough: ', i, arr.length, area);
+    // } else {
+    //   console.log('Long enough: ', i, arr.length, area);
+    //   break;
+    // }
   }
 
-  let extra = arr.length - area;
-  let newArr = arr.splice(0, arr.length - extra);
-  console.log('new arr', newArr.length);
+  extra = arr.length - area;
+  newArr = arr.splice(0, arr.length - extra);
+
   data.data = newArr;
-  console.log(data.data.join(','));
+
   return data;
 
 }
